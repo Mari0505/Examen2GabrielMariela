1cc7651c08d0e8eeeb9246c54bbba8169a2456d3
Mari0505 <103079257+Mari0505@users.noreply.github.com>
2023-11-12 20:09:18 -0600

    None y False sin comillas

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_ObtieneMasBailable (self):
         objeto = MiClase (5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
-        self.assertIsNone(objeto.ObtieneMasBailable([]), "None")  # add assertion here
+        self.assertIsNone(objeto.ObtieneMasBailable([]), None)  # add assertion here
 
     def test_ObtieneMasBailable2(self):
        objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
@@ -30,7 +30,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_VerificaListaCanciones(self):
         objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
-        self.assertIsNotNone (objeto.VerificaListaCanciones([]), "False")  # add assertion here
+        self.assertIsNotNone (objeto.VerificaListaCanciones([]), False)  # add assertion here
 
     def test_VerificaListaCanciones2(self):
         objeto = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
